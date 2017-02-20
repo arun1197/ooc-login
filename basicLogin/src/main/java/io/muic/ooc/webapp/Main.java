7133535e1d3c4a920ed387ba7bad83a79cdb4988
ArunBhusri <abhusri11@gmail.com>
2017-02-20 15:24:56 +0700

    80

--- basicLogin/src/main/java/io/muic/ooc/webapp/Main.java
+++ basicLogin/src/main/java/io/muic/ooc/webapp/Main.java
@@ -19,7 +19,7 @@ public class Main {
 
         String webappDirLocation = "src/main/webapp/";
         Tomcat tomcat = new Tomcat();
-        tomcat.setPort(8080);
+        tomcat.setPort(80);
         StandardContext ctx = (StandardContext) tomcat.addWebapp("/", new File(webappDirLocation).getAbsolutePath());
         System.out.println("configuring app with basedir: " + new File("./" + webappDirLocation).getAbsolutePath());
 
